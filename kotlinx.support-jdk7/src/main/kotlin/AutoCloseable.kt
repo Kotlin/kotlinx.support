a8a8ecd1ad98475c405bb11b2e8e92fd4a8a8c29
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-04-18 23:03:26 +0300

    Rewrite 'use' without finally block. Add docs, catch all Throwables.

--- kotlinx.support-jdk7/src/main/kotlin/AutoCloseable.kt
+++ kotlinx.support-jdk7/src/main/kotlin/AutoCloseable.kt
@@ -1,29 +1,45 @@
 package kotlinx.support.jdk7
 
-
+/**
+ * Executes the given [block] function on this resource and then closes it down correctly whether an exception
+ * is thrown or not.
+ *
+ * @param block a function to process this closable resource.
+ * @return the result of [block] function on this closable resource.
+ */
 public inline fun <T : AutoCloseable, R> T.use(block: (T) -> R): R {
-    var cause: Exception? = null
-    try {
-        return block(this)
-    } catch (e: Exception) {
-        cause = e
-        throw e
-    } finally {
+    val result = try {
+        block(this)
+    } catch (e: Throwable) {
         @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
-        closeQuietly(cause)
+        closeSuppressed(e)
+        throw e
     }
+    close()
+    return result
 }
 
-internal fun AutoCloseable.closeQuietly(cause: Exception?) {
+/**
+ * Closes this [AutoCloseable] suppressing possible exception or error thrown by [AutoCloseable.close] function.
+ * The suppressed exception is added to the list of suppressed exceptions of [cause] exception.
+ */
+internal fun AutoCloseable.closeSuppressed(cause: Throwable) {
     try {
         close()
-    } catch (closeException: Exception) {
-        if (cause != null) {
-            cause.addSuppressed(closeException)
-        }
-        else {
-            throw closeException
-        }
+    } catch (closeException: Throwable) {
+        cause.addSuppressed(closeException)
     }
 }
 
+/*
+// seems to have marginal use cases
+public fun AutoCloseable.closeQuietly(): Throwable? {
+    return try {
+        close()
+        null
+    }
+    catch (e: Throwable) {
+        e
+    }
+}
+*/
