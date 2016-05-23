9798c3b3511919319754c62ab09e91a2736633b8
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-05-23 22:09:01 +0300

    Add some stream utility extensions.

--- kotlinx-support-jdk8/src/test/kotlin/collections/CollectionTest.kt
+++ kotlinx-support-jdk8/src/test/kotlin/collections/CollectionTest.kt
@@ -1,18 +1,16 @@
 package kotlinx.support.jdk8.collections.test
 
 import kotlinx.support.jdk8.collections.*
+import kotlinx.support.jdk8.streams.*
 import kotlinx.support.jdk8.text.*
 import org.junit.Test
 import kotlin.test.*
 import java.util.function.Predicate
 import java.util.stream.Collectors
-import java.util.stream.Stream
 
 class CollectionTest {
 
     val data = listOf("abc", "fo", "baar")
-
-    fun <T> Stream<T>.toList(): List<T> = collect(Collectors.toList<T>())
 
     @Test fun stream() {
         assertEquals(
